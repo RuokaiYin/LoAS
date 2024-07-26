bd9bd0d08e605f9d9287678c439cf381d68baace
Ruokai Yin <ryin25@wisc.edu>
2024-07-26 11:00:27 -0700

    remove all hardware related files

--- src/run_alexnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python3 snn-sparten-result.py --arch alexnet | tee snn_sparten_alexnet_result.txt
-python3 snn-gospa-result.py --arch alexnet | tee snn_gospa_alexnet_result.txt
-python3 snn-loas-mntk-result.py --arch alexnet --loas strong | tee snn_loas-mntk-strong_alexnet_result.txt
-python3 snn-loas-nmtk-result.py --arch alexnet --loas strong | tee snn_loas-nmtk-strong_alexnet_result.txt
-python3 snn-loas-mntk-result.py --arch alexnet --loas normal | tee snn_loas-mntk-normal_alexnet_result.txt
-python3 snn-loas-nmtk-result.py --arch alexnet --loas normal | tee snn_loas-nmtk-normal_alexnet_result.txt
